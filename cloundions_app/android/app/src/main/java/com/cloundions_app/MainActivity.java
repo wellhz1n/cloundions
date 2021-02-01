a83cb0804375c74634abe2e3d2efdd063fd537d0
Wellington Hellstrom Ramos <48489005+wellhz1n@users.noreply.github.com>
2021-02-01 18:20:20 -0300

    first commit

---
+++ cloundions_app/android/app/src/main/java/com/cloundions_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cloundions_app;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "cloundions_app";
+  }
+}
