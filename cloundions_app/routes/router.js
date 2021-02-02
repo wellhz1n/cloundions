35286fa1fd87ae164f6e3fc94fc47f8423f3b15a
Wellington Hellstrom Ramos <48489005+wellhz1n@users.noreply.github.com>
2021-02-01 21:15:21 -0300

    Commit Start

---
+++ cloundions_app/routes/router.js
@@ -0,0 +1,21 @@
+
+import * as React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import Home from '../src/screens/home';
+
+const Stack = createStackNavigator();
+
+const Router = () => {
+    return (<NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+    </NavigationContainer>);
+};
+
+
+export default Router;
---
+++ cloundions_app/src/screens/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>);
+}
+
+export default home;
