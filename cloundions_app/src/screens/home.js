8f9a944795179a5eedf9507ea0614a0d02f20c74
Wellington Hellstrom Ramos <48489005+wellhz1n@users.noreply.github.com>
2021-02-17 10:55:26 -0300

    Mudança do BG

--- cloundions_app/src/screens/home.js
+++ cloundions_app/src/screens/home.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
     View,
+    Dimensions,
     Text,
+    Image,
     ImageBackground,
     StyleSheet,
+    Animated,
     StatusBar,
     SafeAreaView
 } from 'react-native';
@@ -18,49 +21,56 @@ import WeatherCard from '../components/weatherCard.js';
 
 // import { Container } from './styles';
 
-const home = ({ navigation }) => {
+const { height, width } = Dimensions.get('screen');
 
-    navigation.setOptions({ title: 'CASCAVEL' })
+const home = ({ navigation }) => {
 
     return (
         <SafeAreaView style={styles.screen} >
             <StatusBar hidden={true} />
-            <ImageBackground
+            {/* <ImageBackground
                 source={require('../../assets/imgs/bg1.png')}
                 style={styles.imagemFundo}
+            > */}
+            <View style={StyleSheet.absoluteFillObject}>
+
+                <Image
+                    style={[styles.imagemFundo]}
+                    source={require('../../assets/imgs/bg2.png')}
+                    blurRadius={5} />
+            </View>
+            <View
+                style={styles.bodyTemp}
             >
-                <View
-                    style={styles.bodyTemp}
-                >
-                    <View style={styles.informacoesWrapper}>
-                        <InformacoesPrincipais
-                            cidade="Cascavel"
-                            graus="27"
-                            estado="Nublado"
-                        />
-                    </View>
+                <View style={styles.informacoesWrapper}>
+                    <InformacoesPrincipais
+                        cidade="Cascavel"
+                        graus="27"
+                        estado="Nublado"
+                    />
                 </View>
-                <View
-                    style={styles.footerGraph}
-                >
-                    <WeatherCard
-                        Title="Segunda"
-                        Icon="sunny-sharp"
-                        Temperature={30} ></WeatherCard>
-                    <WeatherCard
-                        Title="Terça"
-                        Icon="rainy-sharp"
-                        Temperature={24} ></WeatherCard>
-                    <WeatherCard
-                        Title="Quarta"
-                        Icon="thunderstorm-sharp"
-                        Temperature={20} ></WeatherCard>
-                    <WeatherCard
-                        Title="Quinta"
-                        Icon="cloudy-sharp"
-                        Temperature={18} ></WeatherCard>
-                </View>
-            </ImageBackground>
+            </View>
+            <View
+                style={styles.footerGraph}
+            >
+                <WeatherCard
+                    Title="Segunda"
+                    Icon="sunny-sharp"
+                    Temperature={30} ></WeatherCard>
+                <WeatherCard
+                    Title="Terça"
+                    Icon="rainy-sharp"
+                    Temperature={24} ></WeatherCard>
+                <WeatherCard
+                    Title="Quarta"
+                    Icon="thunderstorm-sharp"
+                    Temperature={20} ></WeatherCard>
+                <WeatherCard
+                    Title="Quinta"
+                    Icon="cloudy-sharp"
+                    Temperature={18} ></WeatherCard>
+            </View>
+            {/* </ImageBackground> */}
         </SafeAreaView >
 
     );
@@ -73,8 +83,9 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     imagemFundo: {
-        flex: 1,
-        justifyContent: 'center'
+        resizeMode: 'cover',
+        height,
+        width,
     },
     bodyTemp: {
         flex: 8,
